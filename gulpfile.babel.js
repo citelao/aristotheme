60040addacc05834ac891f2418b5dd1a5648410a
Ben <ben@stolovitz.com>
2015-10-16 16:58:37 -0500

    also haml rules

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,12 +4,14 @@ import gulp from 'gulp';
 import composer from 'gulp-composer';
 import notify from 'gulp-notify';
 import sass from 'gulp-sass';
+import rename from 'gulp-rename';
+import run from 'gulp-run';
 
 gulp.task('default', ['init', 'build']);
 
 gulp.task('serve', ['css', 'php'], () => {
   gulp.watch('src/sass/**/*.scss', ['css']);
-  gulp.watch('src/**/*.php', ['php']);
+  gulp.watch(['src/**/*.php', 'src/**/*.haml'], ['php']);
 });
 
 gulp.task('init', () => {
@@ -26,8 +28,18 @@ gulp.task('css', () => {
     .pipe(notify('Compiled SCSS.'));
 });
 
-gulp.task('php', () => {
-  return gulp.src(['src/**/*.php'])
+gulp.task('php', ['haml'], () => {
+  return gulp.src(['src/**/*.php', '.tmp/php/**/*.php'])
     .pipe(gulp.dest('dist/'))
     .pipe(notify('Compiled PHP.'));
+});
+
+gulp.task('haml', () => {
+  return gulp.src('src/**/*.haml', { 'read': false })
+    .pipe(run("php build/haml.php <%= file.path.replace(' ', '\\\\ ') %> ", { verbosity: 1 }))
+    .pipe(rename({
+      extname: '.php'
+    }))
+    .pipe(gulp.dest('.tmp/php'))
+    .pipe(notify('Compiled HAML.'));
 });
