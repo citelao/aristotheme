2f14e33a98b54d45b9721339d679c062f9219fad
Ben <ben@stolovitz.com>
2015-11-04 12:17:41 -0600

    added buttons, too

--- dist/front-page.php
+++ dist/front-page.php
@@ -47,15 +47,6 @@
 			</div>
 		</a>
 
-	<?php endforeach; print_r($items); ?>
-
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>	
-		<h2><?php the_title(); ?></h2>
-		<p><?php the_content(); ?></p>
-	<?php endwhile; else : ?>
-		no posts
- 	<?php endif; ?>
-
- 	Home page!
+	<?php endforeach; ?>
 </section>
 <?php get_footer(); ?>	
