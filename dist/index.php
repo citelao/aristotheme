0e6f83206fb6cb451e88a18e4dab5f63cbadcdfb
Ben <ben@stolovitz.com>
2015-11-18 22:40:32 -0600

    more post enhancements

--- dist/index.php
+++ dist/index.php
@@ -1,10 +1,15 @@
 <?php get_header(); ?>
 <ul class="content">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>	
-		<li>
+		<li class="post">
+			<?php 
+				$attachment = get_post_thumbnail_id();
+				$img = wp_get_attachment_image_src($attachment, 'full')[0];
+			?>
+			<!-- <img src="<?php echo $img; ?>" alt="" class="content__image"> -->
 			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<p><?php the_content(''); ?></p>
-			<a href="<?php the_permalink(); ?>" title="Read <?php the_title_attribute(); ?>">Read more&hellip;</a>
+			<a class="post__more" href="<?php the_permalink(); ?>" title="Read <?php the_title_attribute(); ?>">Read more&hellip;</a>
 		</li>
 	<?php endwhile; else : ?>
 		<li>
@@ -19,6 +24,7 @@
 			</p>
 		</li>
  	<?php endif; ?>
+ 	<?php posts_nav_link('', '&laquo; Newer posts', 'Older posts &raquo;'); ?>
 </ul>
 <ul class="sidebar">
 	<?php dynamic_sidebar('bs-sidebar'); ?>
